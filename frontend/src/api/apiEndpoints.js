62934cb6c70df4184c0c43a9d279991c3073feff
Yoiner Duran Rios <yoiner3216898182@gmail.com>
2026-06-17 23:49:19 -0500

    Ajustando frontend en api endpoints

--- frontend/src/api/apiEndpoints.js
+++ frontend/src/api/apiEndpoints.js
@@ -1,8 +1,8 @@
 const API_ENDPOINTS = {
   auth: {
-    login: '/api/auth/token',
-    refresh: '/api/auth/token/refresh',
-    logout: '/api/auth/logout',
+    login: '/api/auth/token/',
+    refresh: '/api/auth/token/refresh/',
+    logout: '/api/auth/logout/',
   },
   user: {
     me: '/api/users/me/',
